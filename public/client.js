26e9e89fe5f7ffca6df8548b2891805191fc2e6a
danijels <73341306+danijels@users.noreply.github.com>
2021-01-29 11:11:13 +0100

    Updates from the challenges

--- public/client.js
+++ public/client.js
@@ -1,8 +1,22 @@
 $(document).ready(function () {
+
+  //This comment right here prevents errors since io is not defined in this file
+  /*Global io*/ 
+  let socket = io();
+  //data === current users, in this case
+  socket.on('user', function(data) {
+    $('#num-users').text(data.currentUsers + ' users online');
+    let message = data.name + data.connected ? ' has joined the chat.' : ' has left the chat.';
+    $('#messages').append($('<li>').html('<b>' + message + '</b>'));
+  });
+
+  socket.on('chat message', (data) => {
+    $('#messages').append($('<li>').text(`${data.name}: ${data.message}`));
+  });
   // Form submittion with new message in field with id 'm'
   $('form').submit(function () {
-    var messageToSend = $('#m').val();
-
+    let messageToSend = $('#m').val();
+    socket.emit('chat message', messageToSend);
     $('#m').val('');
     return false; // prevent form submit from refreshing page
   });
